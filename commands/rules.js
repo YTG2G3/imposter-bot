255e4a2fd6e61648b7cc7b8f476b3bfa1027ab54
Amar Banerjee <abanerjee0032@gmail.com>
2022-04-02 21:36:49 -0700

    rules changes

--- commands/rules.js
+++ commands/rules.js
@@ -1,6 +1,13 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-let rules = "The rules are as follows:\n1. Minimum of 5 players required to play.\n2. Each player will be privately messaged with the role of doctor, sheriff, civlian or mafia";
+let rules = 
+`The rules are as follows:\n
+1. Minimum of 5 players required to play\n
+2. Each player will be privately messaged with the role of doctor, sheriff, civlian or mafia\n
+3. If the player is murdered by the mafia, they must mute\n4. 
+4. Vote is every morning, using replies to the bot\n
+5. If mafia kills all players, they win\n
+6.If players vote out mafia before all are dead, they win`;
 
 module.exports = {
 	data: new SlashCommandBuilder()
