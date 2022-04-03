7ea87f55fd85e857c6c7420a10ddb3182b9bfd6a
YTG2G3 <scratchlover05@gmail.com>
2022-04-02 18:30:07 -0700

    firebase

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 // Load .env
 require('dotenv').config();
 
-// Create new discord bot client
-const { Client, Intents, Collection } = require("discord.js");
-const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES] });
+// Load firebase
+const { initializeApp, applicationDefault } = require('firebase-admin/app');
+initializeApp({ credential: applicationDefault() });
 
-// Load commands
-bot.commands = new Collection();
+// Create new discord bot client
+const { Client, Intents } = require("discord.js");
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES] });
 
 // Bot ready
 bot.on('ready', () => {
     console.log('Bot is ready!');
 });
 
-// Bot received message
-bot.on('message', msg => {
+// Bot received command
+bot.on('interactionCreate', interaction => {
+    if (!interaction.isCommand()) return;
 
+    let cmd = interaction.commandName;
+
+    if (cmd === "open") {
+
+    }
 });
 
 bot.login(process.env.DISCORD_TOKEN);
