0ac72765009e33db0e915c15960680b5efabefcd
Amar Banerjee <abanerjee0032@gmail.com>
2022-04-02 21:37:20 -0700

    change

--- commands/rules.js
+++ commands/rules.js
@@ -4,7 +4,7 @@ let rules =
 `The rules are as follows:\n
 1. Minimum of 5 players required to play\n
 2. Each player will be privately messaged with the role of doctor, sheriff, civlian or mafia\n
-3. If the player is murdered by the mafia, they must mute\n4. 
+3. If the player is murdered by the mafia, they must mute\n 
 4. Vote is every morning, using replies to the bot\n
 5. If mafia kills all players, they win\n
 6.If players vote out mafia before all are dead, they win`;
