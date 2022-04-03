7f14645f1621dfd922831ce2e2a4b7138d6e327b
YTG2G3 <scratchlover05@gmail.com>
2022-04-02 20:16:07 -0700

    firebase

--- index.js
+++ index.js
@@ -2,26 +2,41 @@
 require('dotenv').config();
 
 // Load firebase
-const { initializeApp, applicationDefault } = require('firebase-admin/app');
-initializeApp({ credential: applicationDefault() });
+require('./firebase');
 
 // Create new discord bot client
-const { Client, Intents } = require("discord.js");
+const fs = require('fs');
+const { Client, Intents, Collection } = require("discord.js");
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES] });
 
+// Load commands
+require('./deploy-command');
+
+bot.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (let file of commandFiles) {
+    let command = require(`./commands/${file}`);
+    bot.commands.set(command.data.name, command);
+}
+
 // Bot ready
-bot.on('ready', () => {
+bot.once('ready', () => {
     console.log('Bot is ready!');
 });
 
 // Bot received command
-bot.on('interactionCreate', interaction => {
+bot.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
+    if (interaction.user.bot) return;
 
-    let cmd = interaction.commandName;
+    const cmd = bot.commands.get(interaction.commandName);
+    if (!cmd) return;
 
-    if (cmd === "open") {
-
+    try {
+        await cmd.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
 });
 
