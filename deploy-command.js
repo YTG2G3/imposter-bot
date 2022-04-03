26fa4263de206718ab7d6614762e5cd1ff73f310
Amar Banerjee <abanerjee0032@gmail.com>
2022-04-02 20:52:31 -0700

    made ping

--- deploy-command.js
+++ deploy-command.js
@@ -17,6 +17,7 @@ const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
 (async () => {
     try {
         console.log('Started refreshing application (/) commands.');
+        console.log(commands);
 
         await rest.put(
             Routes.applicationCommands(clientId),
